49f3edadecaa42b25146ed97bf9179fd587d0bfe
Saurav Dhillon <souravdhillon010@gmail.com>
2025-03-05 13:55:49 -0500

    Added functionalDataStructures.js file to practice array methods

---
+++ functionalDataStructures.js
@@ -0,0 +1,5 @@
+const anArrayMixedElements = [1, "hello", 80, "world", 24, "javascript", 6, 99,"LHL", 12, "bootcamp", 45, 3];
+
+const filteredArray = anArrayMixedElements.filter((item) => typeof item === 'number');
+
+console.log(filteredArray);
