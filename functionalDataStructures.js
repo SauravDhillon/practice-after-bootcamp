805df62c2b14f475cc0ed2922e65acce648ee60f
Saurav Dhillon <souravdhillon010@gmail.com>
2025-03-05 14:00:54 -0500

    Implemented .find() method of array

--- functionalDataStructures.js
+++ functionalDataStructures.js
@@ -1,5 +1,11 @@
+// Practicing .filter() array method 
 const anArrayMixedElements = [1, "hello", 80, "world", 24, "javascript", 6, 99,"LHL", 12, "bootcamp", 45, 3];
 
 const filteredArray = anArrayMixedElements.filter((item) => typeof item === 'number');
 
 console.log(filteredArray);
+
+// Practicing .find() method
+const higherThanFifty = filteredArray.find((item) => item > 50);
+
+console.log(higherThanFifty);
