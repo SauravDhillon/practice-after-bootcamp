149f53576577d9ba04b3189693988aa8bc027fc5
Saurav Dhillon <souravdhillon010@gmail.com>
2025-03-13 13:53:48 -0400

    Added useState in the button components

--- fancy-buttons/src/components/TextRepeaterButton.jsx
+++ fancy-buttons/src/components/TextRepeaterButton.jsx
@@ -1,4 +1,8 @@
+import React, { useState } from "react";
+
 function TextRepeaterButton() {
+
+  const [repetitions, setRepetitions] = useState();
   return(
     <button className="TextRepeaterButton">
       <span>I like this text</span>
