f0f802ea1db695d7d87f4889eb303b93ab2e26ad
Saurav Dhillon <souravdhillon010@gmail.com>
2025-03-13 13:22:43 -0400

    Created button components for fancyButtons project and called them in App component

--- fancy-buttons/src/App.jsx
+++ fancy-buttons/src/App.jsx
@@ -1,13 +1,20 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import AngryButton from "./components/AngryButton"
+import CounterButton from './components/CounterButton'
+import LightSwitchButton from './components/LightSwitchButton'
+import TextRepeaterButton from './components/TextRepeaterButton'
 
 function App() {
 
   return (
     <div className="App">
-      
+      <h1>Fancy Buttons!</h1>
+      <section>
+        <AngryButton />
+        <CounterButton />
+        <LightSwitchButton />
+        <TextRepeaterButton />
+      </section>
     </div>
   )
 }
