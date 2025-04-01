143ebb382c19471012546fd2425d57f1a81c3894
Saurav Dhillon <souravdhillon010@gmail.com>
2025-04-01 17:00:16 -0400

    Added ToDoListApp to work on doing full stack with React and express

---
+++ ToDoListApp/backend/index.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const morgan = require('morgan');
+const cors = require('cors');
+
+const port = 8080;
+const app = express();
+
+app.use(morgan('dev'));
+app.use(cors()); // allow requests from any domain
+
+const todoData = {
+  abc: {id: 'abc', task: 'buy milk', done: false},
+  def: {id: 'def', task: 'wash dishes', done: false},
+  ghi: {id: 'ghi', task: 'clean up', done: true},
+};
+
+// GET /api/todos => respond with all todos as JSON
+app.get("/api/todos", (req, res) => {
+  // create an array from the todoData object
+  const data = Object.values(todoData);
+
+  res.json(data);
+});
+
+// DELETE /api/todos => delete the todo associated with the provided id
+app.post("/api/todos/:id/delete", (req, res) => {
+  const todoId = req.params.id;
+  delete todoData[todoId];
+
+  // respond with a generic success message to let the browser know the request succeeded
+  res.json({ success: true });
+});
+
+app.listen(port, () => {
+  console.log(`Server is listening on port ${port}`)
+});
